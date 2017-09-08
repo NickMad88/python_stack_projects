bcd490ccb898be38ffc4eaf8631396ad6329fc19
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-09-07 17:09:02 -0700

    ninja changes

--- disappearing_ninja/server.py
+++ disappearing_ninja/server.py
@@ -4,25 +4,42 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
 @app.route('/ninja')
-def ninja():
+def blahblah():
     return render_template("ninja.html")
 
-@app.route('/ninja/orange')
-def ninjaorange():
-    return render_template("mikey.html")
+@app.route('/ninja/<color>')
+def allninja(color):
 
-@app.route('/ninja/purple')
-def ninjapurple():
-    return render_template("donny.html")
+    if color == 'red':
+        return render_template("raph.html")
+    elif color == 'blue':
+        return render_template("leo.html")
+    elif color == 'orange':
+        return render_template("mikey.html")
+    elif color == 'purple':
+        return render_template("donny.html")
+    elif color == '':
+        return render_template("ninja.html")
+    else:
+        return render_template("not.html")
 
-@app.route('/ninja/red')
-def ninjared():
-    return render_template("raph.html")
+# @app.route('/ninja/orange')
+# def ninjaorange():
+#     return render_template("mikey.html")
 
-@app.route('/ninja/blue')
-def ninjaoblue():
-    return render_template("leo.html")
+# @app.route('/ninja/purple')
+# def ninjapurple():
+#     return render_template("donny.html")
+
+# @app.route('/ninja/red')
+# def ninjared():
+#     return render_template("raph.html")
+
+# @app.route('/ninja/blue')
+# def ninjaoblue():
+#     return render_template("leo.html")
 
 
 
